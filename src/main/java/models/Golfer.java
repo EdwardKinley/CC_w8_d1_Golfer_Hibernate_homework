a1513858d5a98f1aadcecd5d66df563610f6a71e
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-07-09 16:49:09 +0100

    adds to Golfer getters and setters

--- src/main/java/models/Golfer.java
+++ src/main/java/models/Golfer.java
@@ -7,6 +7,7 @@ import javax.persistence.*;
 
 public class Golfer {
 
+    private int id;
     private String firstName;
     private String lastName;
     private int age;
@@ -14,11 +15,32 @@ public class Golfer {
     public Golfer() {}
 
     public Golfer(String firstName, String lastName, int age) {
+        this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
     }
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() { return this.id; }
 
+    public void setId(int id) { this.id = id; }
+
+    @Column(name = "first name")
+    public String getFirstName() { return this.firstName; }
+
+    public void setFirstName(String newFirstName) { this.firstName = newFirstName; }
+
+    @Column(name = "last name")
+    public String getLastName() { return this.lastName; }
+
+    public void setLastName(String newLastName) { this.lastName = newLastName; }
+
+    @Column(name = "age")
+    public int getAge() { return this.age; }
+
+    public void setAge(int newAge) { this.age = newAge; }
 
 }
